a9d126d0bebda87ab7bd2ed4c808d0a918899507
nidhika04 <nidhikachaudhary07@gmail.com>
2026-07-05 21:50:16 +0530

    Update AdapterPatternTest.java

--- DesignPattern/AdapterPattern/AdapterPatternTest.java
+++ DesignPattern/AdapterPattern/AdapterPatternTest.java
@@ -1,1 +1,15 @@
+public class AdapterPatternTest {
 
+    public static void main(String[] args) {
+
+        PaymentProcessor paypal =
+                new PayPalAdapter(new PayPalGateway());
+
+        PaymentProcessor stripe =
+                new StripeAdapter(new StripeGateway());
+
+        paypal.processPayment(5000);
+
+        stripe.processPayment(2500);
+    }
+}
