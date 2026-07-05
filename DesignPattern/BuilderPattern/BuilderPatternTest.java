4061bb8ce1557f2b958a7434a49b85e3b1a45cdd
nidhika04 <nidhikachaudhary07@gmail.com>
2026-07-05 21:29:58 +0530

    Update BuilderPatternTest.java

--- DesignPattern/BuilderPattern/BuilderPatternTest.java
+++ DesignPattern/BuilderPattern/BuilderPatternTest.java
@@ -1,1 +1,24 @@
+public class BuilderPatternTest {
 
+    public static void main(String[] args) {
+
+        Computer gamingPC = new Computer.Builder()
+                .setCPU("Intel Core i9")
+                .setRAM("32GB")
+                .setStorage("1TB SSD")
+                .setGraphicsCard("NVIDIA RTX 4080")
+                .setOperatingSystem("Windows 11")
+                .build();
+
+        Computer officePC = new Computer.Builder()
+                .setCPU("Intel Core i5")
+                .setRAM("16GB")
+                .setStorage("512GB SSD")
+                .setGraphicsCard("Integrated Graphics")
+                .setOperatingSystem("Windows 10")
+                .build();
+
+        gamingPC.display();
+        officePC.display();
+    }
+}
