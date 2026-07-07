3580ac8338a7ba4eebc571705e661a3856be5bea
nidhika04 <nidhikachaudhary07@gmail.com>
2026-07-07 10:01:25 +0530

    Add files via upload

---
+++ DesignPattern/DependencyInjection/CustomerService.java
@@ -0,0 +1,13 @@
+public class CustomerService {
+
+    private CustomerRepository repository;
+
+    // Constructor Injection
+    public CustomerService(CustomerRepository repository) {
+        this.repository = repository;
+    }
+
+    public void getCustomerDetails(int id) {
+        System.out.println(repository.findCustomerById(id));
+    }
+}
---
+++ DesignPattern/DependencyInjection/DependencyInjectionExample.java
@@ -0,0 +1,11 @@
+public class DependencyInjectionExample {
+
+    public static void main(String[] args) {
+
+        CustomerRepository repository = new CustomerRepositoryImpl();
+
+        CustomerService service = new CustomerService(repository);
+
+        service.getCustomerDetails(101);
+    }
+}
