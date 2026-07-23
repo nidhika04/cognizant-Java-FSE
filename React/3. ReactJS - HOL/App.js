224cdb53b1f0d095f67068596542d0d692754cd7
nidhika04 <nidhikachaudhary07@gmail.com>
2026-07-23 20:25:23 +0530

    Create App component with CalculateScore integration

---
+++ React/3. ReactJS - HOL/App.js
@@ -0,0 +1,27 @@
+import CalculateScore from "./Components/CalculateScore";
+
+function App() {
+
+  return (
+
+    <div>
+
+      <CalculateScore
+
+        name="Nivetha"
+
+        school="DNV Public School"
+
+        total={284}
+
+        goal={500}
+
+      />
+
+    </div>
+
+  );
+
+}
+
+export default App;
