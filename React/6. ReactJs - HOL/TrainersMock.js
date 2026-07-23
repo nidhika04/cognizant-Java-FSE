ad61b02ca3602958e76b41a69fa9c5fe493319af
nidhika04 <nidhikachaudhary07@gmail.com>
2026-07-23 20:36:54 +0530

    Add TrainersMock.js with sample trainer data

---
+++ React/6. ReactJs - HOL/TrainersMock.js
@@ -0,0 +1,23 @@
+const trainers = [
+
+    {
+        id:1,
+        name:"John",
+        technology:"React"
+    },
+
+    {
+        id:2,
+        name:"David",
+        technology:"Angular"
+    },
+
+    {
+        id:3,
+        name:"Smith",
+        technology:"Spring Boot"
+    }
+
+];
+
+export default trainers;
